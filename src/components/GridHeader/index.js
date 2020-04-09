7942965c8d038746a08ae10aa10f6edcd2f0db25
Shane Keney <shane@spikeball.com>
2020-04-09 18:55:20 -0400

    - added arrow view change for sorting later

--- src/components/GridHeader/index.js
+++ src/components/GridHeader/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import { orange } from '@material-ui/core/colors';
 
 const GridHeader = props => {
+
+    const [arrowState, setArrowState] = useState(true) // will indicate pointed down
 
     return (
         <div className='section-container'>
             <p className='section'>Image</p>
-            <p className='section'>Name</p>
+
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <p className='section'>Name</p> 
+                
+            { arrowState && <ArrowDropDownIcon style={{color: orange[500]}} fontSize={'small'}  onClick={() => { setArrowState(!arrowState)}} /> }
+            { !arrowState && <ArrowDropUpIcon style={{color: orange[500]}} fontSize={'small'} onClick={() => { setArrowState(!arrowState)}} />}
+            </div>
+
             <p className='section'>Place</p>
             <p className='section'>Email</p>
             <p className='section'>DOB</p>
